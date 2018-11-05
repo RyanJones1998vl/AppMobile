56f4af31fb44c92f9dbfe4f4232e9c68d0b20c65
RyanJones1998vl <35736933+RyanJones1998vl@users.noreply.github.com>
2018-11-05 20:56:51 +0700

    Add files via upload

---
+++ Components/AppTabNavigator/ImageTab.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {TabNavigator} from 'react-navigation'
+import {Icon, Container, Content} from 'native-base';
+
+import CardComponent from '../Card';
+
+class ImageTab extends Component{
+    static navigationOptions={
+        tabBarIcon:({tintColor})=>(
+            <Icon name='eye' style={{color:tintColor}}/>
+        )
+    }
+    render(){
+        return(
+            <Container>
+                <Content>
+                    <CardComponent/ >
+                </Content>
+            </Container>
+        );
+    }
+}
+
+export default ImageTab;
